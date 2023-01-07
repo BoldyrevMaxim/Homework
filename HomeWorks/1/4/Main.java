a49a31e4d754b60cdbf22e715e1c91276d87b1ca
BoldyrevMaxim <122152379+BoldyrevMaxim@users.noreply.github.com>
2023-01-07 13:42:01 +0300

    Create Main.java

---
+++ HomeWorks/1/4/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static boolean Comparison(int a,int b){
+        return((a+b>=10) && (a+b<=20));
+
+    }
+    public static void main(String[] args) {
+        System.out.println(Comparison(3,4));
+
+
+    }
+}

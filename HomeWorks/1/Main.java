599af845e0db7131d537a88e92e194930604af38
BoldyrevMaxim <122152379+BoldyrevMaxim@users.noreply.github.com>
2023-01-07 13:39:13 +0300

    Create Main.java

---
+++ HomeWorks/1/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+     public static double Func(int a, int b, int c, int d) {
+         return (a * (b + (c / d)));
+     }
+    public static void main(String[] args) {
+         System.out.println(Func(2,5,7,8));
+
+
+    }
+ }

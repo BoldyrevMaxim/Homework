3a9326dad3773950b67f101f7641ae4c9e57540f
BoldyrevMaxim <122152379+BoldyrevMaxim@users.noreply.github.com>
2023-01-07 15:20:36 +0300

    Update Main.java

--- HomeWorks/6/Main.java
+++ HomeWorks/6/Main.java
@@ -1,97 +1,88 @@
 import java.util.Random;
-import java.util.Scanner;
 
-abstract class Animal {
-    protected String name;
-    protected double jumpWeight;
-    protected double runLength;
-    protected double swimLength;
-    public static Random rand = new Random();
-    String[] name_anim = {"Мрак", "Мурка", "Шерлок", "Рекс", "Бобик", "Карасик", "Рыжий"};
-    int index = rand.nextInt(name_anim.length);
-    String random_name = name_anim[index];
+public class Main {
+    public static void main(String[] args) {
+        Cat[] cats = new Cat[5];
+        Dog[] dogs = new Dog[5];
 
-    public Animal(String name, double jumpWeight, double runLength, double swimLength){
-        this.name = name;
-        this.jumpWeight = jumpWeight;
-        this.runLength = runLength;
-        this.swimLength = swimLength;
+        for (int i = 0; i < 5; i++){
+            cats[i] = new Cat();
+            dogs[i] = new Dog();
+        }
+
+        for (Cat cat: cats) {
+            cat.printAnimalInfo();
+            System.out.println(cat.jump(3.25));
+            System.out.println(cat.swim(1));
+        }
+
+        for (Dog dog: dogs) {
+            dog.printAnimalInfo();
+            System.out.println(dog.jump(3));
+            System.out.println(dog.swim(10));
+        }
+    }
+}
+
+class Animal {
+    Random random = new Random();
+    protected double jumpLimit;
+    protected double runLimit;
+    protected double swimLimit;
+
+    public Animal(double jumpLimit, double runLimit, double swimLimit) {
+        this.jumpLimit = jumpLimit;
+        this.runLimit = runLimit;
+        this.swimLimit = swimLimit;
     }
 
     public Animal() {
-        this.name = random_name;
+        this.jumpLimit = 3;
+        this.runLimit = 350;
+        this.swimLimit = 10;
     }
 
-    public void animalInfo(){
-        System.out.println("Кличка: " + this.name);
-        System.out.println("Высота прыжка:" + this.jumpWeight);
-        System.out.println("Длина пробежки:" + this.runLength);
-        System.out.println("Длина возможного проплыва:" + this.swimLength);
+    public void printAnimalInfo() {
+        System.out.println("Jump Limit: " + this.jumpLimit);
+        System.out.println("Run Limit: " + this.runLimit);
+        System.out.println("Swim Limit: " + this.swimLimit);
     }
 
-    public boolean jump(double distance){
-        return this.jumpWeight >= distance;
+    public boolean jump(double jumpHeight) {
+        return jumpHeight <= this.jumpLimit;
     }
 
-    public boolean run(double distance){
-        return this.runLength >= distance;
+    public boolean run(double runLength) {
+        return runLength <= this.runLimit;
     }
 
-    public boolean swim(double distance){
-        return this.swimLength >= distance;
+    public boolean swim(double swimLength) {
+        return swimLength <= this.swimLimit;
     }
 }
 
 class Dog extends Animal{
-    public Dog(){
-        super();
-        this.jumpWeight = rand.nextDouble(0.00001, 0.5);
-        this.runLength = rand.nextDouble(1, 500);
-        this.swimLength = rand.nextDouble(1, 10);
+    public Dog(double jumpLimit, double runLimit, double swimLimit) {
+        super(jumpLimit, runLimit, swimLimit);
     }
 
-    public Dog(String name, double jumpWeight, double runLength, double swimLength){
-        super(name, jumpWeight, runLength, swimLength);
+    public Dog() {
+        super();
+        this.jumpLimit = this.random.nextDouble(1.5, 3.25);
+        this.runLimit = this.random.nextDouble(525.25, 645.5);
+        this.swimLimit = this.random.nextDouble(15.5, 35.5);
     }
 }
 
-class Cat extends Animal{
-    public Cat(){
+class Cat extends Animal {
+    public Cat(double jumpLimit, double runLimit, double swimLimit) {
+        super(jumpLimit, runLimit, swimLimit);
+    }
+
+    public Cat() {
         super();
-        this.jumpWeight = rand.nextDouble(0.1, 2);
-        this.runLength = rand.nextDouble(1, 200);
-        this.swimLength = 0;
-    }
-
-    public Cat(String name, double jumpWeight, double runLength, double swimLength){
-        super(name, jumpWeight, runLength, swimLength);
-    }
-}
-
-public class Main{
-    public static Scanner inp = new Scanner(System.in);
-    public static void main(String[] args) {
-        System.out.println("Введите кол-во животных (кол-во каждого вида одинаково (при 1 будет 1 кот и 1 собака)): ");
-        int amount_animals = inp.nextInt();
-        Dog[] dogs = new Dog[amount_animals];
-        Cat[] cats = new Cat[amount_animals];
-
-
-        for (int i = 0; i < amount_animals; i++) {
-            dogs[i] = new Dog();
-            cats[i] = new Cat();
-        }
-
-        for (int i = 0; i < amount_animals; i++) {
-            dogs[i].animalInfo();
-            System.out.println(dogs[i].jump(10.1));
-            System.out.println("---------------------------------");
-        }
-
-        for (int i = 0; i < amount_animals; i++) {
-            cats[i].animalInfo();
-            System.out.println(cats[i].run(4.8));
-            System.out.println("---------------------------------");
-        }
+        this.jumpLimit = this.random.nextDouble(1, 5);
+        this.runLimit = this.random.nextDouble(200, 400);
+        this.swimLimit = 0;
     }
 }
